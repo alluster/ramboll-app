964c56c482fbe7fea955b1f4606bd113be270058
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2019-05-24 18:30:34 +0300

    initial commit

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import LinkHOC from '../LinkHOC';
 
 const Nav = () => {
 	return (
-		<nav className="navbar ">
+		<nav className="navbar navbar-default">
 		<div className="container-fluid">
 			<div className="navbar-header">
 			<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Nav from './../components/Nav'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LinkHOC from '../components/LinkHOC';
-
+import Container from '../components/Container';
 const Home = () =>  {
   return (
     <div className="home">
   
       <header className="home-header">
+      <Container>
       <h1>Ramboll Expertice Management Platform</h1>
       <h3>Building teams for complex projects with specific competence.</h3>
         <br/>
@@ -15,6 +16,8 @@ const Home = () =>  {
             <button className="btn btn-primary btn-lg" >Join the platform </button>
         </LinkHOC>
   
+      </Container>
+
       </header>
     </div>
   );
